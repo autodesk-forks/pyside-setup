fb46079fc42b038795816aacc23d517c148334dd
Bang Nguyen <bang.nguyen@autodesk.com>
2019-05-30 13:06:51 -0400

    Update PySide version

--- scripts/adsk_maya_package_pyside2_lnx.sh
+++ scripts/adsk_maya_package_pyside2_lnx.sh
@@ -5,14 +5,14 @@ fi
 
 # TODO Break out the Qt version number from the prefix. We need the version # separate for a directory name (see below)
 if [[ -z "${QTVERSION}" ]]; then
-	echo "QTVERSION is undefined.  Example: export QTVERSION=qt_5.12.4"
+	echo "QTVERSION is undefined.  Example: export QTVERSION=5.12.4"
 	exit 1
 else
 	echo "QTVERSION=${QTVERSION}"
 fi
 
 if [[ -z "${PYSIDEVERSION}" ]]; then
-	echo "PYSIDEVERSION is undefined.  Example: export PYSIDEVERSION=pyside_5.12.4"
+	echo "PYSIDEVERSION is undefined.  Example: export PYSIDEVERSION=5.12.4"
 	exit 1
 else
 	echo "PYSIDEVERSION=${PYSIDEVERSION}"
@@ -20,7 +20,7 @@ fi
 
 export WORKDIR=$1
 export SRCDIR=$WORKDIR/src
-export INSTALLDIR=$WORKDIR/install/$PYSIDEVERSION
+export INSTALLDIR=$WORKDIR/install/pyside_$PYSIDEVERSION
 
 PKGDIR="$PWD/build/lib.linux-*-2.7"
 mkdir -p $INSTALLDIR
@@ -28,8 +28,8 @@ cp -R $PKGDIR/PySide2 $INSTALLDIR/PySide2
 pushd $PKGDIR >/dev/null
 tar zcf $INSTALLDIR/PySide2/pyside2uic.tar.gz pyside2uic
 popd >/dev/null
-tar zcf $INSTALLDIR/PySide2/pyside2-$QTVERSION-include.tgz pyside2_install/py2.7-qt5.12.4-64bit-release/include/PySide2
-tar zcf $INSTALLDIR/PySide2/shiboken2-$QTVERSION-include.tgz pyside2_install/py2.7-qt5.12.4-64bit-release/include/shiboken2
+tar zcf $INSTALLDIR/PySide2/pyside2-qt$QTVERSION-include.tgz pyside2_install/py2.7-qt5.12.4-64bit-release/include/PySide2
+tar zcf $INSTALLDIR/PySide2/shiboken2-qt$QTVERSION-include.tgz pyside2_install/py2.7-qt5.12.4-64bit-release/include/shiboken2
 cp pyside2_install/py2.7-qt5.12.4-64bit-release/bin/pyside2-uic $INSTALLDIR/PySide2
 echo "==== Success ===="
 
