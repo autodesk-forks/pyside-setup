4dd21737ce87770af20cc091e656e4f38aae4596
Keith Kyzivat <keith.kyzivat@qt.io>
2022-03-04 15:32:29 -0500

    Import of Maya build scripts, update for PySide6
    Import and port of the Maya PySide build scripts from
    adsk-maya-pyside-5.15.2 to PySide6 - from v6.2.3 tag.
    (which is now tracked in a new branch named adsk-maya-pyside-6.2.3)
    
    Dropped Python 3.7.7 support, since Maya 2024 will not be supporting it.

---
+++ autodesk-maya-scripts/adsk_maya_build_pyside6_osx.sh
@@ -0,0 +1,153 @@
+# Exit bash script if return codes from commands are nonzero
+set -u
+
+# Parameter 1 - Absolute path to workspace directory
+if [ $# -eq 0 ]; then
+    echo "Need to pass workspace directory to the script"
+    exit 1
+fi
+
+# Environment Variable - QTVERSION - Version of Qt used to build PySide6
+if [[ -z "${QTVERSION}" ]]; then
+    echo "QTVERSION is undefined. Example: export QTVERSION=6.2.3"
+    exit 1
+else
+    echo "QTVERSION=${QTVERSION}"
+fi
+
+# Environment Variable - PYSIDEVERSION - Version of PySide6 built
+if [[ -z "${PYSIDEVERSION}" ]]; then
+    echo "PYSIDEVERSION is undefined. Example: export PYSIDEVERSION=6.2.3"
+    exit 1
+else
+    echo "PYSIDEVERSION=${PYSIDEVERSION}"
+fi
+
+# Environment Variable - PYTHONVERSION - Version of Python for which PySide6 is built
+if [[ -z "$PYTHONVERSION" ]]; then
+    echo "PYTHONVERSION is undefined. Example: export PYTHONVERSION=3.9.5"
+    exit 1
+else
+    echo "PYTHONVERSION=${PYTHONVERSION}"
+fi
+
+# Extract MAJOR(A), MINOR(B), and REVISION(C) from PYTHONVERSION
+PYTHONVERSION_ARRAY=($(echo $PYTHONVERSION | tr "." "\n"))
+PYTHONVERSION_A=${PYTHONVERSION_ARRAY[0]}
+PYTHONVERSION_B=${PYTHONVERSION_ARRAY[1]}
+PYTHONVERSION_C=${PYTHONVERSION_ARRAY[2]}
+
+# Define Python Version Shortcuts (AB and A.B)
+PYTHONVERSION_AB=${PYTHONVERSION_A}${PYTHONVERSION_B}
+PYTHONVERSION_AdotB=${PYTHONVERSION_A}.${PYTHONVERSION_B}
+
+# Validate that the Python version given is within the accepted values
+if [[ ! ("$PYTHONVERSION_A" == "3") ]]; then
+    echo "Python major version should be '3'. Example: export PYTHONVERSION=3.9.7"
+    exit 1
+fi
+
+# Python 3.9.5 artifacts don't have any pymalloc suffix, but future python builds might. Leaving this in place.
+export PYMALLOC_SUFFIX=
+
+# Location of the workspace directory (root)
+export WORKSPACE_DIR=$1
+
+# Exit bash script if expanding vars that were never set.
+set -e
+
+# Location of external dependencies directory
+export EXTERNAL_DEPENDENCIES_DIR=$WORKSPACE_DIR/external_dependencies
+
+# Location of Qt build directory (in external dependencies)
+export QTPATH=$EXTERNAL_DEPENDENCIES_DIR/qt_$QTVERSION
+
+# Location of liblang directory (in external dependencies)
+export CLANG_INSTALL_DIR=$EXTERNAL_DEPENDENCIES_DIR/libclang
+
+# Name of the Python executable
+export PYTHON_EXE=python${PYTHONVERSION_AdotB}
+
+
+# Create qt.conf file
+touch $EXTERNAL_DEPENDENCIES_DIR/qt_$QTVERSION/bin/qt.conf
+echo [Paths] > $EXTERNAL_DEPENDENCIES_DIR/qt_$QTVERSION/bin/qt.conf
+echo Prefix=.. >> $EXTERNAL_DEPENDENCIES_DIR/qt_$QTVERSION/bin/qt.conf
+
+# Get the number of processors available to build PySide6
+export NUMBER_OF_PROCESSORS=`sysctl -n hw.ncpu`
+
+# Ensure that pip and its required modules are installed
+$PYTHON_EXE -m ensurepip
+$PYTHON_EXE -m pip install pip
+$PYTHON_EXE -m pip install setuptools
+$PYTHON_EXE -m pip install wheel==0.34.1
+$PYTHON_EXE -m pip install packaging
+
+# Note: Mac uses the python.org distribution of Python 3, so there is no need to modify `slots`.
+
+# Cleanup PREFIX and DIST dirs. See below for their definition.
+export PREFIX_DIR=$WORKSPACE_DIR/build
+export DIST_DIR=$WORKSPACE_DIR/dist
+for BUILDTYPE in release debug;
+do
+    if [ -e "${PREFIX_DIR}/${BUILDTYPE}" ]; then
+        rm -rf "${PREFIX_DIR}/${BUILDTYPE}"
+    fi
+
+    if [ -e "${DIST_DIR}/${BUILDTYPE}" ]; then
+        rm -rf "${DIST_DIR}/${BUILDTYPE}"
+    fi
+done
+
+for BUILDTYPE in release debug;
+do
+    export BUILDTYPE_STR="Release"
+    export EXTRA_SETUP_PY_OPTS=""
+    if [ "$BUILDTYPE" == "debug" ]; then
+        export BUILDTYPE_STR="Debug"
+        export EXTRA_SETUP_PY_OPTS="--debug"
+    fi
+
+    # By default, the pyside6-uic and pyside6-rcc wrappers are installed in the Python directory during the install step.
+    # Using the --prefix option, we specify a different location where to output the files, which makes it easier to copy
+    # the wrappers in the /bin folder when packaging.
+    export PREFIX_DIR_BUILDTYPE="${PREFIX_DIR}/${BUILDTYPE}"
+    mkdir -p "$PREFIX_DIR_BUILDTYPE"
+
+    # Location where the built wheels will be outputted
+    # To be able to use pyside6-uic and pyside6-rcc, we need the metadata from the .dist-info folders,
+    # which can be obtained by unpacking the wheels.
+    export DIST_DIR_BUILDTYPE="${DIST_DIR}/${BUILDTYPE}"
+    mkdir -p "$DIST_DIR_BUILDTYPE"
+
+    $PYTHON_EXE setup.py install --qmake=$QTPATH/bin/qmake --ignore-git --parallel=$NUMBER_OF_PROCESSORS --prefix=$PREFIX_DIR_BUILDTYPE $EXTRA_SETUP_PY_OPTS
+    if [ $? -eq 0 ]; then
+        echo "==== Success ==== $BUILDTYPE_STR Build"
+    else
+        echo "**** Failed to build **** $BUILDTYPE_STR Build"
+        exit 1
+    fi
+
+    $PYTHON_EXE setup.py bdist_wheel --qmake=$QTPATH/bin/qmake --ignore-git --parallel=$NUMBER_OF_PROCESSORS --dist-dir=$DIST_DIR_BUILDTYPE $EXTRA_SETUP_PY_OPTS
+    if [ $? -eq 0 ]; then
+        echo "==== Success ==== $BUILDTYPE_STR Build Wheel"
+    else
+        echo "**** Failed to build **** $BUILDTYPE_STR Build Wheel"
+        exit 1
+    fi
+
+    # Unpack the wheels
+    export WHEEL_SUFFIX=${QTVERSION}-${PYSIDEVERSION}-cp${PYTHONVERSION_AB}-cp${PYTHONVERSION_AB}${PYMALLOC_SUFFIX}
+
+    # Python 3.9.X was built with SDK version 10.14. Newer Python versions might be different.
+    export WHEEL_SUFFIX=${WHEEL_SUFFIX}-macosx_10_14_x86_64
+    export PYSIDE6_WHEEL=PySide6-${WHEEL_SUFFIX}.whl
+    export SHIBOKEN6_WHEEL=shiboken6-${WHEEL_SUFFIX}.whl
+    export SHIBOKEN6_GEN_WHEEL=shiboken6_generator-${WHEEL_SUFFIX}.whl
+
+    $PYTHON_EXE -m wheel unpack "${DIST_DIR_BUILDTYPE}/${PYSIDE6_WHEEL}" --dest="${DIST_DIR_BUILDTYPE}"
+    $PYTHON_EXE -m wheel unpack "${DIST_DIR_BUILDTYPE}/${SHIBOKEN6_WHEEL}" --dest="${DIST_DIR_BUILDTYPE}"
+    $PYTHON_EXE -m wheel unpack "${DIST_DIR_BUILDTYPE}/${SHIBOKEN6_GEN_WHEEL}" --dest="${DIST_DIR_BUILDTYPE}"
+done
+echo "==== Finished ===="
---
+++ autodesk-maya-scripts/adsk_maya_package_pyside6_osx.sh
@@ -0,0 +1,170 @@
+# Exit bash script if return codes from commands are nonzero
+set -e
+
+if [ ! -e README.pyside2.md ] ; then
+    echo "Pyside2 packaging script not in correct current directory"
+    echo "ABORTING: Current directory incorrect."
+    exit 1
+fi
+
+# Parameter 1 - Absolute path to workspace directory
+if [ $# -eq 0 ]; then
+    echo "Need to pass workspace directory to the script"
+	exit 1
+fi
+
+# Environment Variable - QTVERSION - Version of Qt used to build PySide2
+if [[ -z "$QTVERSION" ]]; then
+	echo "QTVERSION is undefined. Example: export QTVERSION=5.15.2"
+	exit 1
+else
+	echo "QTVERSION=$QTVERSION"
+fi
+
+# Environment Variable - PYSIDEVERSION - Version of PySide2 built
+if [[ -z "$PYSIDEVERSION" ]]; then
+	echo "PYSIDEVERSION is undefined. Example: export PYSIDEVERSION=5.15.2"
+	exit 1
+else
+	echo "PYSIDEVERSION=$PYSIDEVERSION"
+fi
+
+# Environment Variable - PYTHONVERSION - Version of Python for which PySide2 is built
+if [[ -z "$PYTHONVERSION" ]]; then
+    echo "PYTHONVERSION is undefined. Example: export PYTHONVERSION=3.7.7"
+    exit 1
+else
+    echo "PYTHONVERSION=${PYTHONVERSION}"
+fi
+
+# Extract MAJOR(A), MINOR(B), and REVISION(C) from PYTHONVERSION
+PYTHONVERSION_ARRAY=($(echo $PYTHONVERSION | tr "." "\n"))  
+PYTHONVERSION_A=${PYTHONVERSION_ARRAY[0]}
+PYTHONVERSION_B=${PYTHONVERSION_ARRAY[1]}
+PYTHONVERSION_C=${PYTHONVERSION_ARRAY[2]}
+
+# Define Python Version Shortcuts (AB and A.B)
+PYTHONVERSION_AB=${PYTHONVERSION_A}${PYTHONVERSION_B}
+PYTHONVERSION_AdotB=${PYTHONVERSION_A}.${PYTHONVERSION_B}
+
+# Validate that the Python version given is within the accepted values
+if [[ ! ("$PYTHONVERSION_A" == "2" || "$PYTHONVERSION_A" == "3") ]]; then
+    echo "Python major version should be '2' or '3'. Example: export PYTHONVERSION=3.7.7"
+    exit 1
+fi
+
+
+# Location of the workspace directory (root), made absolute so RPATHs can be detected properly.
+export WORKSPACE_DIR=$(python3 -c "import os,sys; print(os.path.abspath(sys.argv[1]))" $1)
+
+# Exit bash script if the script expands variables that were never set.
+set -u
+
+# Location of the install directory within the workspace (where the builds will be located)
+export INSTALL_DIR="$WORKSPACE_DIR/install"
+
+# Location of the pyside2-uic and pyside2-rcc wrappers (determined by the --prefix option in the build script)
+export PREFIX_DIR="$WORKSPACE_DIR/build"
+
+# Location of the pyside2-uic and pyside2-rcc .dist-info metadata folders (determined by the --dist-dir option in the build script)
+export DIST_DIR="$WORKSPACE_DIR/dist"
+
+mkdir -p "$INSTALL_DIR"
+
+# Determine if it is a Python 2 or Python 3 build
+export PATH_TO_MAYAPY_REGEX='1s/.*/\#\!\/usr\/bin\/env python2_unused_in_maya2022plus_on_macOS/'
+if [ -e "pyside3_install" ]; then
+    export PATH_TO_MAYAPY_REGEX='1s/.*/\#\!\/usr\/bin\/env mayapy/'
+fi
+echo "Built for Python $PYTHONVERSION_A"
+
+
+
+# Write PySide2 build information to a "pyside2_version" file 
+# instead of encoding the pyside version number in a directory name
+cat <<EOF >"$INSTALL_DIR/pyside2_version"
+pyside2 $PYSIDEVERSION
+qt $QTVERSION
+python version $PYTHONVERSION_A
+EOF
+
+# Copy, setup and package
+for BUILDTYPE in release debug
+do
+    export DEBUG_SUFFIX=""
+	if [ "$BUILDTYPE" == "debug" ]; then
+        export DEBUG_SUFFIX="d"
+    fi
+
+    export BUILDTYPE_INSTALL_DIR="pyside${PYTHONVERSION_A}${DEBUG_SUFFIX}_install"
+
+    if [[ -e "$BUILDTYPE_INSTALL_DIR" ]]; then
+        echo "Copying the build (release, debug) to the installation directory"
+        cp -R "$BUILDTYPE_INSTALL_DIR" "$INSTALL_DIR/"
+
+        # Define the path to the directory of the PySide2 build (bin, include, lib, share) in the installation directory
+        export PYSIDE2_ROOT_DIR="$INSTALL_DIR/$BUILDTYPE_INSTALL_DIR/py${PYTHONVERSION_AdotB}-qt${QTVERSION}-64bit-${BUILDTYPE}"
+
+        # Workaround: Since the pyside2-uic and pyside2-rcc wrappers are not installed in the build directory, we need to copy them from
+        # the --prefix directory into the artifact's /bin folder
+        for wrapper in pyside2-rcc pyside2-uic
+        do
+            echo "Copying \"$PREFIX_DIR/$BUILDTYPE/bin/$wrapper\" to \"$PYSIDE2_ROOT_DIR/bin/\""
+            cp "$PREFIX_DIR/$BUILDTYPE/bin/$wrapper" "$PYSIDE2_ROOT_DIR/bin/"
+
+            # Replace interpreter path for relative path to mayapy
+            echo "Updating $wrapper script's shebang line with regex $PATH_TO_MAYAPY_REGEX"
+            sed -i -e "$PATH_TO_MAYAPY_REGEX" "$PYSIDE2_ROOT_DIR/bin/$wrapper"
+        done
+
+        # Copy the .dist-info metadata folders, since the pyside2-uic and pyside2-rcc wrappers rely on [console_scripts] entrypoints.
+        for distfolder in PySide2 shiboken2 shiboken2_generator
+        do
+            echo "Copying $distfolder-PYSIDEVERSION.dist-info to .../site-packages"
+            cp -R "$DIST_DIR/$BUILDTYPE/$distfolder-$PYSIDEVERSION/$distfolder-$PYSIDEVERSION.dist-info" "$PYSIDE2_ROOT_DIR/lib/python$PYTHONVERSION_AdotB/site-packages/$distfolder-$PYSIDEVERSION.dist-info"
+            
+            echo "Renaming $distfolder-PYSIDEVERSION.dist-info/RECORD so package cannot be pip uninstalled"
+            mv "$PYSIDE2_ROOT_DIR/lib/python$PYTHONVERSION_AdotB/site-packages/$distfolder-$PYSIDEVERSION.dist-info/RECORD" "$PYSIDE2_ROOT_DIR/lib/python$PYTHONVERSION_AdotB/site-packages/$distfolder-$PYSIDEVERSION.dist-info/RECORD-DONOTUNINSTALL"
+        done
+
+        # Copy uic and rcc executable files into site-packages/PySide2, since it is the first search location for pyside2-uic that is used by loadUiType.
+        for sitepackagesfile in rcc uic
+        do
+            cp "$PREFIX_DIR/$BUILDTYPE/lib/python$PYTHONVERSION_AdotB/site-packages/PySide2/$sitepackagesfile" "${PYSIDE2_ROOT_DIR}/lib/python$PYTHONVERSION_AdotB/site-packages/PySide2/$sitepackagesfile"
+        done
+        
+        # Remove incorrect RPATHs of installed site-packages/PySide2/uic
+        install_name_tool -delete_rpath "$WORKSPACE_DIR/external_dependencies/qt_$QTVERSION/lib" -delete_rpath '@loader_path' "${PYSIDE2_ROOT_DIR}/lib/python$PYTHONVERSION_AdotB/site-packages/PySide2/uic"
+
+        # Copy the 'scripts' PySide2 sudmodules folder manually, since the pyside2-uic and pyside2-rcc wrappers invoke
+        # pyside_tool.py through [console_scripts] entrypoints.
+        echo "Copying site-packages/PySide2/scripts from $PREFIX_DIR to $PYSIDE2_ROOT_DIR"
+        cp -R "$PREFIX_DIR/$BUILDTYPE/lib/python$PYTHONVERSION_AdotB/site-packages/PySide2/scripts" "$PYSIDE2_ROOT_DIR/lib/python$PYTHONVERSION_AdotB/site-packages/PySide2/"
+
+        # Workaround: Until the issue is addressed within PySide2 build scripts, we manually copy the 'support' PySide2 submodule
+        # into the build to prevent the "__feature__ could not be imported" error.
+        echo "Copying site-packages/PySide2/support from $PREFIX_DIR to $PYSIDE2_ROOT_DIR"
+        cp -R "$PREFIX_DIR/$BUILDTYPE/lib/python$PYTHONVERSION_AdotB/site-packages/PySide2/support" "$PYSIDE2_ROOT_DIR/lib/python$PYTHONVERSION_AdotB/site-packages/PySide2/"
+
+        # Delete __pycache folders
+        echo "Deleting __pycache__ folders from install"
+        find "$PYSIDE2_ROOT_DIR/lib/python$PYTHONVERSION_AdotB/site-packages/PySide2/" -type d -name "__pycache__" -exec rm -r {} +
+
+        # Change RPATHs of shiboken2 executable
+        echo "install_name_tool -delete_rpath \"$WORKSPACE_DIR/external_dependencies/libclang/lib\" \"$PYSIDE2_ROOT_DIR/bin/shiboken2\""
+        install_name_tool -delete_rpath "$WORKSPACE_DIR/external_dependencies/libclang/lib" "$PYSIDE2_ROOT_DIR/bin/shiboken2"
+        echo "install_name_tool -delete_rpath \"$WORKSPACE_DIR/external_dependencies/qt_$QTVERSION/lib\" \"$PYSIDE2_ROOT_DIR/bin/shiboken2\""
+        install_name_tool -delete_rpath "$WORKSPACE_DIR/external_dependencies/qt_$QTVERSION/lib" "$PYSIDE2_ROOT_DIR/bin/shiboken2"
+        set +e # don't bail if commands return nonzero exit codes, since we're explicitly checking this one.
+        echo "install_name_tool -add_rpath @loader_path/../MacOS \"$PYSIDE2_ROOT_DIR/bin/shiboken2\""
+        install_name_tool -add_rpath @loader_path/../MacOS "$PYSIDE2_ROOT_DIR/bin/shiboken2"
+        if [[ $? -ne 0 ]]; then
+            echo "**** Error: Failed setting rpath. Calling otool -l */shiboken2"
+            set -e
+            echo "otool -l \"$PYSIDE2_ROOT_DIR/bin/shiboken2\""
+            otool -l "$PYSIDE2_ROOT_DIR/bin/shiboken2"
+        fi
+        set -e
+    fi
+done
+echo "==== Success ===="
