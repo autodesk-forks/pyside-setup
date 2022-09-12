e9481040a5a33b1c06a3d30831421cc95efbe2bf
Keith Kyzivat <keith.kyzivat@qt.io>
2022-09-12 11:08:19 -0400

    Linux build: 5.15.2.1 support, Stricter reqs checking, pip requirements.txt
    * Handle PySide versions that have a 4th segment to the version number.
    * Use a python executable that is passed into the build script.
      This makes it more
      useful to the publicly published version of the Maya pyside-setup
      repository.
    * Remove old unnecessary slots hack fix (not needed for any Python
      builds we have available now)
    
    Bash Script Safety and stricter requirements checking:
    * Bash script safety
      set -e -u to catch errors early and terminate (terminating when
      derefing undefined variables and when subprocesses return nonzero exit
      codes).
    * Validate that the script is run from the intended path.
    * Report errors to stderr.
    * Accept python 2.7.x, 3.9.5 or 3.9.7, reject others - matching intended
      supported Python releases.
    * Support PYSIDEVERSION env var "Preflight" value, assume it matches
      QTVERSION in this case. Jenkins will set PYSIDEVERSION to "Preflight"
      if it cannot determine the pyside version from the branch name.
    * Prerequisites - make this closer to how Coin builds PySide2:
      + Use OS-provided clang, llvm, and cmake packages instead of artifacts
        if RHEL8.
      + Use requirements.txt to install required python modules.

--- autodesk-maya-scripts/adsk_maya_build_pyside2_lnx.sh
+++ autodesk-maya-scripts/adsk_maya_build_pyside2_lnx.sh
@@ -1,35 +1,80 @@
-# Parameter 1 - Absolute path to workspace directory
-if [ $# -eq 0 ]; then
-    echo "Need to pass workspace directory to the script"
+#!/usr/bin/env bash
+
+set -e # Terminate with failure if any command returns nonzero
+set -u # Terminate with failure any time an undefined variable is expanded
+
+if [[ ! -f README.pyside2.md ]]; then
+    echo >&2 "Please execute from the root of the pyside-setup repository."
     exit 1
 fi
 
+# Parameter 1 - Absolute path to workspace directory
+if [[ $# -eq 0 ]]; then
+    echo >&2 "Need to pass workspace directory to the script"
+    exit 1
+fi
+
+set +u
 # Environment Variable - QTVERSION - Version of Qt used to build PySide2
 if [[ -z "${QTVERSION}" ]]; then
-    echo "QTVERSION is undefined. Example: export QTVERSION=5.15.2"
+    echo >&2 "QTVERSION is undefined. Example: export QTVERSION=5.15.2"
     exit 1
 else
     echo "QTVERSION=${QTVERSION}"
 fi
 
 # Environment Variable - PYSIDEVERSION - Version of PySide2 built
-if [[ -z "${PYSIDEVERSION}" ]]; then
-    echo "PYSIDEVERSION is undefined. Example: export PYSIDEVERSION=5.15.2"
-    exit 1
-else
-    echo "PYSIDEVERSION=${PYSIDEVERSION}"
+if [[ -z "${PYSIDEVERSION}" || "${PYSIDEVERSION^^}" == "PREFLIGHT" ]]; then
+    # Assume PYSIDEVERSION matches QTVERSION, as it should anyway...
+    export PYSIDEVERSION=$QTVERSION
 fi
+if [[ ! ${PYSIDEVERSION} =~ ^[0-9]{1,2}\.[0-9]{1,3}\.[0-9]{1,3}(\.[1-9])?$ ]]; then
+    echo "PYSIDEVERSION is invalid. It should be a version number. Example: export PYSIDEVERSION=5.15.2"
+    exit 1
+fi
+echo "PYSIDEVERSION=${PYSIDEVERSION}"
+
+# Check for OS version in the same way that Jenkinsfile does.
+OS=
+distro=$(hostnamectl | awk '/Operating System/ { print $3 }')
+if [[ "$distro" =~ "CentOS" ]]; then
+    OS="centos7"
+else
+    # Assuming RHEL8
+    OS="rhel"
+fi
+
+for pythonexe in ${PYTHONEXE} ${PYTHONDEXE}; do
+    pythonexe_varname="PYTHONEXE"
+    pythonconfig="RelWithDebInfo"
+    if [[ "$pythonexe" == "$PYTHONDEXE" ]]; then
+        pythonexe_varname="PYTHONDEXE"
+        pythonconfig="Debug"
+    fi
+
+    # Make sure the user has passed in a python executable to use
+    if [[ -z "$pythonexe" || ! -e "$pythonexe" ]]; then
+        if [[ -z "$pythonexe" ]]; then echo -n "${pythonexe_varname} is undefined. "; fi
+        if [[ ! -e "$pythonexe" ]]; then echo -n "${pythonexe} doesn't exist. "; fi
+        if [[ ! -x "$pythonexe" ]]; then echo -n "${pythonexe} isn't executable. "; fi
+        echo "Example: export ${pythonexe_varname}=$1/external_dependencies/cpython/3.9.5/${pythonconfig}/bin/python3.9"
+        exit 1
+    else
+        echo "${pythonexe_varname}=${pythonexe}"
+    fi
+
+    pythonexe_version=$($pythonexe -c "import sys; v=sys.version_info; print('{}.{}.{}'.format(v.major, v.minor, v.micro))")
+    if [[ ! "$pythonexe_version" == "$PYTHONVERSION" ]]; then
+        echo >&2 "Expecting Python ${PYTHONVERSION}, but the python executable ${pythonexe} is ${pythonexe_version}. aborting."
+        exit 1
+    fi
+done
 
 # Environment Variable - PYTHONVERSION - Version of Python for which PySide2 is built
-if [[ -z "$PYTHONVERSION" ]]; then
-    echo "PYTHONVERSION is undefined. Example: export PYTHONVERSION=3.7.7"
-    exit 1
-else
-    echo "PYTHONVERSION=${PYTHONVERSION}"
-fi
+echo "PYTHONVERSION=${PYTHONVERSION}"
 
 # Extract MAJOR(A), MINOR(B), and REVISION(C) from PYTHONVERSION
-PYTHONVERSION_ARRAY=($(echo $PYTHONVERSION | tr "." "\n"))  
+PYTHONVERSION_ARRAY=($(echo $PYTHONVERSION | tr "." "\n"))
 PYTHONVERSION_A=${PYTHONVERSION_ARRAY[0]}
 PYTHONVERSION_B=${PYTHONVERSION_ARRAY[1]}
 PYTHONVERSION_C=${PYTHONVERSION_ARRAY[2]}
@@ -39,8 +84,12 @@ PYTHONVERSION_AB=${PYTHONVERSION_A}${PYTHONVERSION_B}
 PYTHONVERSION_AdotB=${PYTHONVERSION_A}.${PYTHONVERSION_B}
 
 # Validate that the Python version given is within the accepted values
-if [[ ! ("$PYTHONVERSION_A" == "2" || "$PYTHONVERSION_A" == "3") ]]; then
-    echo "Python major version should be '2' or '3'. Example: export PYTHONVERSION=3.7.7"
+if [[ ! "$PYTHONVERSION" =~ (2\.7\.1[0-9]|3\.9\.5|3\.9\.7) ]]; then
+    # We expect the python version to be 2.7.10+, 3.9.5 or 3.9.7 right now. It
+    # will change in the future, and at that time this check should be updated
+    # to reflect the newly supported python versions.
+    echo >&2 "Expecting Python 2.7.1?, 3.9.5 or 3.9.7. aborting."
+    echo >&2 "Example: export PYTHONVERSION=3.9.7"
     exit 1
 fi
 
@@ -60,22 +109,15 @@ export EXTERNAL_DEPENDENCIES_DIR=$WORKSPACE_DIR/external_dependencies
 # Location of Qt build directory (in external dependencies)
 export QTPATH=$EXTERNAL_DEPENDENCIES_DIR/qt_$QTVERSION
 
-# Location of liblang directory (in external dependencies)
-export CLANG_INSTALL_DIR=$EXTERNAL_DEPENDENCIES_DIR/libclang
+if [[ "$OS" == "centos7" ]]; then
+    # Location of liblang directory (in external dependencies)
+    # Qt for Python will look for this environment variable.
+    # Under RHEL8, PySide2 should be able to find LLVM from the path.
+    export CLANG_INSTALL_DIR=$EXTERNAL_DEPENDENCIES_DIR/libclang
 
-# Location of CMake directory (in external dependencies)
-# Latest CMake version in CentOS 7.6 is 2.8.x.x, but PySide2 requires a minimum of CMake 3.1 
-export CMAKE_DIR=$EXTERNAL_DEPENDENCIES_DIR/cmake-3.13.3-Linux-x86_64/bin
-
-# Location of Python directory (in external dependencies)
-export PYTHONEXE_DIR=$EXTERNAL_DEPENDENCIES_DIR/cpython/${PYTHONVERSION}/RelWithDebInfo/bin
-export PYTHONEXE_D_DIR=$EXTERNAL_DEPENDENCIES_DIR/cpython/${PYTHONVERSION}/Debug/bin
-
-# Name of the Python executable
-if [ $PYTHONVERSION_A -eq 3 ]; then
-    export PYTHON_EXE=python${PYTHONVERSION_AdotB}
-else
-    export PYTHON_EXE=python-bin
+    # Location of CMake directory (in external dependencies)
+    # Latest CMake version in CentOS 7.6 is 2.8.x.x, but PySide2 requires a minimum of CMake 3.1
+    export PATH=$EXTERNAL_DEPENDENCIES_DIR/cmake-3.13.3-Linux-x86_64/bin:$PATH
 fi
 
 # Cleanup PREFIX and DIST dirs. See below for their definition.
@@ -83,13 +125,13 @@ export PREFIX_DIR=$WORKSPACE_DIR/build
 export DIST_DIR=$WORKSPACE_DIR/dist
 for BUILDTYPE in release debug;
 do
-	if [ -e "${PREFIX_DIR}/${BUILDTYPE}" ]; then
-	    rm -rf "${PREFIX_DIR}/${BUILDTYPE}"
-	fi
+    if [ -e "${PREFIX_DIR}/${BUILDTYPE}" ]; then
+        rm -rf "${PREFIX_DIR}/${BUILDTYPE}"
+    fi
 
-	if [ -e "${DIST_DIR}/${BUILDTYPE}" ]; then
-	    rm -rf "${DIST_DIR}/${BUILDTYPE}"
-	fi
+    if [ -e "${DIST_DIR}/${BUILDTYPE}" ]; then
+        rm -rf "${DIST_DIR}/${BUILDTYPE}"
+    fi
 done
 
 
@@ -98,79 +140,62 @@ touch $EXTERNAL_DEPENDENCIES_DIR/qt_$QTVERSION/bin/qt.conf
 echo [Paths] > $EXTERNAL_DEPENDENCIES_DIR/qt_$QTVERSION/bin/qt.conf
 echo Prefix=.. >> $EXTERNAL_DEPENDENCIES_DIR/qt_$QTVERSION/bin/qt.conf
 
-# Store current PATH to be able to restore it later
-export OLDPATH=$PATH
-
 # Get the number of processors available to build PySide2
 export NUMBER_OF_PROCESSORS=`cat /proc/cpuinfo | grep processor | wc -l`
 
 
 for BUILDTYPE in release debug;
 do
-    export BUILDTYPE_STR="Release"
-    export PYTHONEXEPATH=$PYTHONEXE_DIR
-    export EXTRA_SETUP_PY_OPTS=""
-    export DEBUG_SUFFIX=
-
-	if [ "$BUILDTYPE" == "debug" ]; then
-	    export BUILDTYPE_STR="Debug"
-        export PYTHONEXEPATH=$PYTHONEXE_D_DIR
-		export EXTRA_SETUP_PY_OPTS="--debug"
+    if [ "$BUILDTYPE" == "debug" ]; then
+        export BUILDTYPE_STR="Debug"
+        export PYTHON_EXE=$PYTHONDEXE
+        export EXTRA_SETUP_PY_OPTS="--debug"
         export DEBUG_SUFFIX=d
-	fi
+    else
+        export BUILDTYPE_STR="Release"
+        export PYTHON_EXE=$PYTHONEXE
+        export EXTRA_SETUP_PY_OPTS=""
+        export DEBUG_SUFFIX=
+    fi
 
     # By default, the pyside2-uic and pyside2-rcc wrappers are installed in the Python directory during the install step.
     # Using the --prefix option, we specify a different location where to output the files, which makes it easier to copy 
     # the wrappers in the /bin folder when packaging.
-	export PREFIX_DIR_BUILDTYPE="${PREFIX_DIR}/${BUILDTYPE}"
-	mkdir -p "$PREFIX_DIR_BUILDTYPE"
+    export PREFIX_DIR_BUILDTYPE="${PREFIX_DIR}/${BUILDTYPE}"
+    mkdir -p "$PREFIX_DIR_BUILDTYPE"
 
     # Location where the built wheels will be outputted
-    # To be able to use pyside2-uic.exe and pyside2-rcc.exe, we need the metadata from the .dist-info folders, 
+    # To be able to use pyside2-uic.exe and pyside2-rcc.exe, we need the metadata from the .dist-info folders,
     # which can be obtained by unpacking the wheels.
-	export DIST_DIR_BUILDTYPE="${DIST_DIR}/${BUILDTYPE}"
-	mkdir -p "$DIST_DIR_BUILDTYPE"
+    export DIST_DIR_BUILDTYPE="${DIST_DIR}/${BUILDTYPE}"
+    mkdir -p "$DIST_DIR_BUILDTYPE"
 
-    # Add Python executable to the PATH
-    export PATH=$PYTHONEXEPATH:$OLDPATH
+    # Ensure that pip and its required modules are installed for Python 3 (release version)
+    $PYTHON_EXE -m ensurepip
+    $PYTHON_EXE -m pip install --upgrade pip
 
-    if [ $PYTHONVERSION_A -eq 3 ]; then    
-        # Ensure that pip and its required modules are installed for Python 3 (release version)
-        $PYTHON_EXE -m ensurepip
-        $PYTHON_EXE -m pip install pip
-        $PYTHON_EXE -m pip install setuptools
-
-        if [ "$BUILDTYPE" == "release" ]; then
-            # Maya redefines `slots` so it is called `slots_` in the file. Rename it so it can be detected again
-            sed -i -e 's/\(PyType_Slot\ \*slots\)_/\1/' $EXTERNAL_DEPENDENCIES_DIR/cpython/${PYTHONVERSION}/RelWithDebInfo/include/python${PYTHONVERSION_AdotB}${PYMALLOC_SUFFIX}/object.h
-        fi
-    fi
-
-    $PYTHON_EXE -m pip install wheel==0.34.1
+    # Now install all required Python modules for building from pyside-setup's requirements.txt file.
+    $PYTHON_EXE -m pip install -r requirements.txt
     $PYTHON_EXE -m pip install packaging
-
-    # Add CMake (>3.1) to the PATH to ensure it is chosen first instead of default version 2.8.x.x of CentOS 7
-    export PATH=$CMAKE_DIR:$PATH
 
     # Build PySide2
     $PYTHON_EXE setup.py install --qmake=$QTPATH/bin/qmake --ignore-git --parallel=$NUMBER_OF_PROCESSORS --prefix=$PREFIX_DIR_BUILDTYPE $EXTRA_SETUP_PY_OPTS
     if [ $? -eq 0 ]; then
         echo "==== Success ==== $BUILDTYPE_STR Build"
     else
-        echo "**** Failed to build **** $BUILDTYPE_STR Build"
+        echo >&2 "**** Failed to build **** $BUILDTYPE_STR Build"
         exit 1
     fi
     $PYTHON_EXE setup.py bdist_wheel --qmake=$QTPATH/bin/qmake --ignore-git --parallel=$NUMBER_OF_PROCESSORS --dist-dir=$DIST_DIR_BUILDTYPE $EXTRA_SETUP_PY_OPTS
     if [ $? -eq 0 ]; then
         echo "==== Success ==== $BUILDTYPE_STR Build Wheel"
     else
-        echo "**** Failed to build **** $BUILDTYPE_STR Build Wheel"
+        echo >&2 "**** Failed to build **** $BUILDTYPE_STR Build Wheel"
         exit 1
     fi
 
     # Unpack the wheels
-    export WHEEL_SUFFIX=${QTVERSION}-${PYSIDEVERSION}-cp${PYTHONVERSION_AB}-cp${PYTHONVERSION_AB}
-
+    export WHEEL_SUFFIX=${PYSIDEVERSION}-${QTVERSION}-cp${PYTHONVERSION_AB}-cp${PYTHONVERSION_AB}
     if [ $PYTHONVERSION_A -eq 3 ]; then
         export WHEEL_SUFFIX=${WHEEL_SUFFIX}${DEBUG_SUFFIX}${PYMALLOC_SUFFIX}-linux_x86_64
     else
